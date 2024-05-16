81ba87f49a0f23ef2a32a66a0f9caa90da1bbe0b
Pablo Sangueza <pablo.sangueza@parkmobile.io>
2024-05-16 10:59:55 -0400

    navigation added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' } // Redirect to login page by default
+
 ];
 
 @NgModule({
